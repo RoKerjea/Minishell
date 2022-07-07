7a9253568218800884f3c54b583c1e2c579a7b2d
RoKerjea <rokerjea@student.42.fr>
2022-07-07 18:21:16 +0200

    V0.30 by rokerjea at 07 07 2022 18h21 CEST

---
+++ parsing/print_token.c
@@ -0,0 +1,17 @@
+//TEST FT only needed to check step result, to delete!
+void	print_token(t_tok_list *list)
+{
+	t_tok_link	*link;
+	int	i;
+
+	i = 1;
+	link = list->first;
+	printf("Token list len = %d\n", list->len);
+	printf("Token list after separator in one char* by token :\n");
+	while (link != NULL)
+	{
+		printf("Token n%d is of type %d, str == (%s) \n", i, link->meta, link->str);
+		link = link->next;
+		i++;
+	}
+}
--- parsing/tokenizer.c
+++ parsing/tokenizer.c
@@ -6,14 +6,17 @@
 /*   By: rokerjea <rokerjea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 19:54:43 by rokerjea          #+#    #+#             */
-/*   Updated: 2022/07/03 19:31:08 by rokerjea         ###   ########.fr       */
+/*   Updated: 2022/07/07 18:14:28 by rokerjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/macro.h"
 
-//FILE: what's needed to make list of tokens from input, depending of their type
+// FILE: what's needed to make list of tokens from input, depending of their type
+// INPUT: the full line from readline (xx)
+// OUTPUT: liste chainee de token contenant seulement les '|', str pour cmd,
+// et redirections avec leurs cibles
 
 /*
 typedef struct s_tok_link
@@ -39,44 +42,26 @@ check_errors for metastr incoherences(during metaparse? after all tokens are cre
 
 //  |  < et << peuvent etres colles a des str et continuer a fonctionner, > >> problement aussi
 
-//mais si metachars are sequentials in a wrong way, message "process : parse error near (symbol that's a problem)"
-// zsh: parse error near `>'
+// mais si metachars are sequentials in a wrong way, message "process : parse error near (symbol that's a problem)"
+//  zsh: parse error near `>'
 
-//TEST FT only needed to check step result, to delete!
-void	print_token(t_tok_list *list)
+// verif step for token, if a redirection doesn't have a target, problem
+// syntax check happen BEFORE var expansion! so, last step of tokenizer
+int syntax_checker(t_tok_list *list)
 {
-	t_tok_link	*link;
-	int	i;
-
-	i = 1;
-	link = list->first;
-	printf("Token list len = %d\n", list->len);
-	printf("Token list after separator in one char* by token :\n");
-	while (link != NULL)
-	{
-		printf("Token n%d is of type %d, str == (%s) \n", i, link->meta, link->str);
-		link = link->next;
-		i++;
-	}
-}
-
-//verif step for token, if a redirection doesn't have a target, problem
-//syntax check happen BEFORE var expansion!
-int	meta_check_arg(t_tok_list	*list)
-{
-	t_tok_link	*link;
+	t_tok_link *link;
 
 	link = list->first;
 	while (link != NULL)
 	{
 		if ((link->meta == IN || link->meta == OUT) && ft_strlen(link->str) < 2)
 		{
-			printf ("minishell: syntax error near unexpected token `%c'\n", link->next->str[0]);//!oups, what if link-nextn'existe pas?
+			printf("minishell: syntax error near unexpected token `%c'\n", link->next->str[0]); //! oups, what if link-nextn'existe pas?
 			return (NO);
 		}
 		if ((link->meta == HEREDOC || link->meta == APPEND) && ft_strlen(link->str) < 3)
 		{
-			printf ("minishell: syntax error near unexpected token `%c'\n", link->next->str[0]);
+			printf("minishell: syntax error near unexpected token `%c'\n", link->next->str[0]);
 			return (NO);
 		}
 		link = link->next;
@@ -84,26 +69,24 @@ int	meta_check_arg(t_tok_list	*list)
 	return (YES);
 }
 
-//start of tokenizer, create list of token, launch token separator,
-//check result of this step (if redirection have their arguments)
-t_tok_list	*tokenizerstart(char *input)
+// start of tokenizer, create list of token, launch token separator,
+// check result of this step (if redirection have their arguments)
+t_tok_list *tokenizerstart(char *input)
 {
-	t_tok_list	*list;
-	
-	list = make_list();
-	
-	printf ("len of input == %lu\n", strlen(input));//TEST to delete
-	sep_token(input, list);
-	if (meta_check_arg(list) == NO)
-		printf("problem and should free list and stop current input\n");//TEST to delete
-	//print_token(list);
-	return (list);
+	t_tok_list *token_list;
+
+	token_list = make_list();
+	printf("len of input == %lu\n", strlen(input)); // TEST to delete
+	sep_token(input, token_list);
+	if (syntax_checker(token_list) == NO)
+		printf("problem and should free list and stop current input\n"); // TEST to delete, but replace with a prob manager
+	return (token_list);
 }
 
-//create the list, no links in yet
+// create the list, no links in yet
 t_tok_list *make_list()
 {
-	t_tok_list	*list;
+	t_tok_list *list;
 
 	list = malloc(sizeof(t_tok_list));
 	list->len = 0;
@@ -112,10 +95,10 @@ t_tok_list *make_list()
 	return (list);
 }
 
-//create a new empty link and add it at the last place in the list
+// create a new empty link and add it at the last place in the list
 t_tok_link *make_add_link(t_tok_list *list)
 {
-	t_tok_link	*link;
+	t_tok_link *link;
 
 	link = malloc(sizeof(t_tok_link));
 	if (list->len == 0)
@@ -134,36 +117,36 @@ t_tok_link *make_add_link(t_tok_list *list)
 	return (link);
 }
 
-//strparser et metaparser cree le maillon, et malloc la bonne taille pour la str 
-//strndup(str, len)
-void	sep_token(char *str, t_tok_list *list)
+// strparser et metaparser cree le maillon, et malloc la bonne taille pour la str
+// strndup(str, len)
+void sep_token(char *str, t_tok_list *list)
 {
-	int	i;
-	
+	int i;
+
 	i = 0;
-	printf("strart of tokenzer\n");//TEST to delete
-	printf ("//input== \"%s\"\n", str);//TEST to delete
+	printf("strart of tokenzer\n");	   // TEST to delete
+	printf("//input== \"%s\"\n", str); // TEST to delete
 	while (str[i] != '\0')
 	{
 		while (ft_isspace(str[i]) == YES && str[i] != '\0')
 			i++;
-		if (is_meta (str[i]) == NO && str[i] != '\0')
+		if (is_meta(str[i]) == NO && str[i] != '\0')
 		{
-			printf("start of strsep for char %d\n", i);//TEST to delete
-			i += strparser(list, str + i); //manage start and end of quotes as a single block of str
-			printf("end of strsep for char %d\n", i);//TEST to delete
+			printf("start of strsep for char %d\n", i); // TEST to delete
+			i += strparser(list, str + i);				// manage start and end of quotes as a single block of str
+			printf("end of strsep for char %d\n", i);	// TEST to delete
 		}
-		else if (is_meta (str[i]) == YES && str[i] != '\0')
+		else if (is_meta(str[i]) == YES && str[i] != '\0')
 		{
-			printf("start of metasep for char %d\n", i);//TEST to delete
+			printf("start of metasep for char %d\n", i); // TEST to delete
 			i += metaparser(list, str + i);
-			printf("end of metasep for char %d\n", i);//TEST to delete
+			printf("end of metasep for char %d\n", i); // TEST to delete
 		}
 	}
 }
 
-//check if char input is a metachar, to be treated differently (need to move it elsewere)
-int	is_meta(char c)
+// check if char input is a metachar, to be treated differently (need to move it elsewere)
+int is_meta(char c)
 {
 	if (c == '|' || c == '<' || c == '>')
 		return (YES);
@@ -171,9 +154,9 @@ int	is_meta(char c)
 		return (NO);
 }
 
-//check if char input is a space, to be treated differently (need to move it elsewere)
-//space, tab, newline and possibly carriage return.
-int	ft_isspace(char c)
+// check if char input is a space, to be treated differently (need to move it elsewere)
+// space, tab, newline and possibly carriage return.
+int ft_isspace(char c)
 {
 	if (c == ' ' || c == '\t')
 		return (YES);
@@ -181,45 +164,45 @@ int	ft_isspace(char c)
 		return (NO);
 }
 
-//get str until EOL or metachar, presumably part of same cmd for exec
-int	strparser(t_tok_list *list, char *str)
+// get str until EOL or metachar, presumably part of same cmd for exec
+int strparser(t_tok_list *list, char *str)
 {
-	int	i;
+	int i;
 	t_tok_link *link;
 
 	i = 0;
 	link = make_add_link(list);
-	while (is_meta (str[i]) == NO && str[i] != '\0')
+	while (is_meta(str[i]) == NO && str[i] != '\0')
 	{
 		if (str[i] == '\'' || str[i] == '\"')
 			i += find_end_quote(str + i, str[i]);
 		else
 			i++;
 	}
-	printf ("str in strparse == \"%s\", strlen = %d\n", str, i);//TEST to delete
+	printf("str in strparse == \"%s\", strlen = %d\n", str, i); // TEST to delete
 	link->str = ft_strndup(str, i);
-	printf ("str made == \"%s\"\n", link->str);//TEST to delete
+	printf("str made == \"%s\"\n", link->str); // TEST to delete
 	link->meta = CMD;
 	return (i);
 }
 
-//get str starting from metachar->create a token with metachar and redirection target, if any,
-int	metaparser(t_tok_list *list, char *str)
+// get str starting from metachar->create a token with metachar and redirection target, if any,
+int metaparser(t_tok_list *list, char *str)
 {
-	int	i;
+	int i;
 	t_tok_link *link;
 
 	i = 0;
 	link = make_add_link(list);
 	i += metachar_parser(str);
-	printf ("str in metaparse == \"%s\", strlen = %d\n", str, i);//TEST to delete
+	printf("str in metaparse == \"%s\", strlen = %d\n", str, i); // TEST to delete
 	link->str = ft_strndup(str, i);
 	link->meta = meta_type(link->str);
 	return (i);
 }
 
-//identify the type of redirection the current token is
-int	meta_type(char *str)
+// identify the type of redirection the current token is
+int meta_type(char *str)
 {
 	if (strncmp(str, "<<", 2) == 0)
 		return (HEREDOC);
@@ -235,21 +218,21 @@ int	meta_type(char *str)
 		return (0);
 }
 
-//find & return the length of the str between quotes c
-int	find_end_quote(char *str, char c)
+// find & return the length of the str between quotes c
+int find_end_quote(char *str, char c)
 {
-	int		i;
-	
-	printf ("gate quote %s\n", str);//TEST to delete
+	int i;
+
+	printf("gate quote %s\n", str); // TEST to delete
 	i = 1;
 	while (str[i] != c && str[i] != '\0')
 		i++;
 	return (i + 1);
 }
 
-//fnd & return the length of meta token, from the metachar,
-//to the end of it's arg, if any
-int	metachar_parser(char *str)
+// fnd & return the length of meta token, from the metachar,
+// to the end of it's arg, if any
+int metachar_parser(char *str)
 {
 	if (strncmp(str, "<<", 2) == 0 || strncmp(str, ">>", 2) == 0)
 		return (meta_and_arg_size(str + 2) + 2);
@@ -261,10 +244,10 @@ int	metachar_parser(char *str)
 		return (0);
 }
 
-//find & return the length of arg of redirection token
-int	meta_and_arg_size(char *str)//could be fused later with str parser?
+// find & return the length of arg of redirection token
+int meta_and_arg_size(char *str) // could be fused later with str parser?
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (ft_isspace(str[i]) == YES && str[i] != '\0')
@@ -274,9 +257,9 @@ int	meta_and_arg_size(char *str)//could be fused later with str parser?
 		if (str[i] == '\'' || str[i] == '\"')
 			i += find_end_quote(str + i, str[i]);
 		else
-			i++;		
+			i++;
 	}
-	return (i);	
+	return (i);
 }
 
 /*
@@ -307,7 +290,7 @@ maybe in struct of link?
 
 need to parse >> before >
 and << before <
-not separate automatically every metachar from char immediatly next? NEED TESTS!! 
+not separate automatically every metachar from char immediatly next? NEED TESTS!!
 */
 
 /*
