39bb4f85a1ec734e9728f42f29157069bcaed395
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-09-30 08:10:01 +0200

    add cmd_num + del execute

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 02:12:12 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/09/29 21:59:03 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/09/30 08:09:29 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,6 @@
 # include "common.h"
 # include "minishell.h"
 # include <sys/types.h>
-
-/* enum e_redir_type
-{
-	IN,
-	OUT,
-	APPEND
-}; */
 
 enum e_fd_dup_type
 {
@@ -40,13 +33,9 @@ typedef struct s_list_info
 	int				rfds[2];
 	int				pfds[3];
 	int				status;
+	unsigned int	cmd_num;
 }	t_list_info;
 
-// SIMULATE_PARSER_ARGS -> will be deleted
-t_parsed_cmd	*create_pipeline(int argc, char *argv[], char *const envp[]);
-void		print_cmd_list(t_parsed_cmd *cmd_list);
-
-// envp will be replaced everywhere by t_env
 t_list_info	*cmd_list_info(t_parsed_cmd *cmd_list);
 void		free_cmd_list(t_parsed_cmd *cmd_list);
 int			exec_controller(t_list_info *list_info, t_env *local_env);
@@ -55,11 +44,10 @@ int			exec_redirect(t_parsed_cmd *cmd_list, t_list_info *list_info);
 void		exec_cmd(char **cmd, char **envp);
 void		print_err_open(int errnum, char *path);
 void		print_err(int errnum, char *str, int line);
-char		**get_paths(char *const envp[]); // to be replaced by t_env
+char		**get_paths(char *const envp[]);
 void		free_err_join(char *path, char *tmp, char **paths);
 void		free_tab(char **tab);
-int			exec_subshell(t_parsed_cmd *cmd, t_list_info *info, t_env *local_env);
+void		exec_subshell(t_parsed_cmd *cmd, t_list_info *info, t_env *l_env);
 void		exec_pipeline(t_parsed_cmd *cmd, t_list_info *info);
-int			execute(t_parsed_cmd *cmd, t_list_info *info, t_env *local_env);
 
 #endif
