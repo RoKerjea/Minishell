caa4a361b1a09eef2c882d775ee0bbcdce67c728
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-09-30 08:08:41 +0200

    builtin integration into pipeline

--- exec/exec_subshell.c
+++ exec/exec_subshell.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 00:08:30 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/09/29 22:00:54 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/09/30 01:02:47 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,17 @@
 #include "../include/utils.h"
 #include "../include/macro.h"
 
-// int	execute(t_parsed_cmd *cmd, t_list_info *info, t_env *local_env)
-// {
-// 	int	status;
-// 	char	**envp;
-
-// 	envp = make_env_tab(local_env);//to make char **env for execve
-// 	status = 0;
-// 	if (cmd->exec_type == BUILT)
-// 		status = exec_builtin(cmd, local_env);
-// 	else
-// 		exec_subshell(cmd, info, envp);
-// 	return (exit(127), status);
-// }
-
-int	exec_subshell(t_parsed_cmd *cmd, t_list_info *info, t_env *local_env)
+void	exec_subshell(t_parsed_cmd *cmd, t_list_info *info, t_env *local_env)
 {
-	int		status;
 	char	**envp;
 
 	envp = make_env_tab(local_env);//to make char **env for execve
-	status = 0;
 	if (info->size > 1)
 		exec_pipeline(cmd, info);
 	if (exec_redirect(cmd, info))
 		exit(EXIT_FAILURE); // handle errno + err_status
 	if (cmd->exec_type == BUILT)
-		status = exec_builtin(cmd, local_env);
+		exit(exec_builtin(cmd, local_env));
 	else
 		exec_cmd(cmd->cmd_args, envp);
 	if (errno == -1 || errno == ENOENT)
@@ -52,5 +36,4 @@ int	exec_subshell(t_parsed_cmd *cmd, t_list_info *info, t_env *local_env)
 	if (errno == EACCES || errno == EISDIR
 		|| errno == EPERM || errno == ENOEXEC || errno == EINVAL)
 		exit(126);
-	return (status);
 }
