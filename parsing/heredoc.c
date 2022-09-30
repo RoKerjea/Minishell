cbc5ec441c6fdccdae645b0cb065111e0136b164
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-09-30 08:10:42 +0200

    fix EOF + signals (exit to implement)

--- parsing/heredoc.c
+++ parsing/heredoc.c
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rokerjea <rokerjea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 23:16:11 by rokerjea          #+#    #+#             */
-/*   Updated: 2022/09/24 22:51:15 by rokerjea         ###   ########.fr       */
+/*   Updated: 2022/09/30 07:59:01 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/parsing.h"
+#include "../include/common.h"
+#include "../include/utils.h"
+#include <signal.h>
 
 char	*find_free_name(char *str)
 {
@@ -49,6 +52,50 @@ char	*findnewname(char *name)
 	return (res);
 }
 
+// char	*heredoc(t_tok_link *link, t_env *local_env)
+// {
+// 	int		filefd;
+// 	char	*filepath;
+// 	char	*expanded;
+// 	char	*input;
+
+// 	filepath = findnewname(link->str[0]);
+// 	filefd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 	//protect
+// 	write(STDERR_FILENO, "> ", 2);
+// 	input = get_next_line(STDIN_FILENO);
+// 	signal(SIGINT, sigint_hd_handler);
+// 	while (input && strcmp(input, link->str[0]) != 0 && errno != EINTR)
+// 	{
+// 		if (link->meta == HEREDOC && ft_strchr(input, '$'))
+// 		{
+// 			expanded = str_expander(input, local_env, 0);
+// 			//protect
+// 			free (input);
+// 			input = expanded;
+// 		}
+// 		write(filefd, input, strlen(input));
+// 		free (input);
+// 		if (errno == EINTR)
+// 		{
+// 			free (input);
+// 			close(filefd);
+// 			close(STDIN_FILENO);
+// 		}
+// 		write(STDERR_FILENO, "> ", 2);
+// 		input = get_next_line(STDIN_FILENO);
+// 		if (errno == EINTR)
+// 		{
+// 			free (input);
+// 			close(filefd);
+// 			close(STDIN_FILENO);
+// 		}
+// 		write(filefd, "\n", 1);
+// 	}
+// 	close (filefd);
+// 	signal(SIGINT, SIG_IGN);
+// 	return (filepath);
+// }
 
 //need expand step, which need env
 char	*heredoc(t_tok_link *link, t_env *local_env)
@@ -62,7 +109,8 @@ char	*heredoc(t_tok_link *link, t_env *local_env)
 	filefd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	//protect
 	input = readline ("> ");
-	while (strcmp(input, link->str[0]) != 0)
+	signal(SIGINT, sigint_handler);
+	while (input && strcmp(input, link->str[0]) != 0 && errno != EINTR)
 	{
 		if (link->meta == HEREDOC && ft_strchr(input, '$'))
 		{
@@ -77,9 +125,10 @@ char	*heredoc(t_tok_link *link, t_env *local_env)
 		write(filefd, "\n", 1);
 	}
 	close (filefd);
+	signal(SIGINT, SIG_IGN);
 	return (filepath);
 }
-/* 
+/*
 int	main(int ac, char** av)
 {
 	int		filefd;
